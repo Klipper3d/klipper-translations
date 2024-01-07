50eeca499c59596135768879f4a374772daf3c09
alfrix <alfredomonclus@gmail.com>
2024-01-07 07:42:17 -0300

    fix scripts lint issues

--- scripts/update-md.sh
+++ scripts/update-md.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Automatically generate/update markdown files for 
+# Automatically generate/update markdown files for
 # Localization files in the ./locales/[language]/ folder
 # Prerequisit: pip install mdpo
 
@@ -8,23 +8,23 @@ width=25564
 po2md -v
 
 for dir in docs/locales/*/; do
-  
+
   dir=${dir%*/}
   dir=${dir##*/}
-  
+
   echo "Updating $dir"
   for file in docs/*.md; do
-    
+
     mdfile=${file/docs\//} #Remove docs/
-    mdfilepath=docs/$mdfile #add docs/ 
-    
+    mdfilepath=docs/$mdfile #add docs/
+
     if [ $dir = "en" ]; then
       pofile=${mdfile//.md/.pot} #replace .md with .pot
     else
       pofile=${mdfile//.md/.po} #replace .md with .po
     fi
 
-    pofilepath=docs/locales/$dir/LC_MESSAGES/$pofile #Add target directory 
+    pofilepath=docs/locales/$dir/LC_MESSAGES/$pofile #Add target directory
     #to pofile
     targetmdfilepath=docs/locales/$dir/$mdfile #Add target directory to mdfile
 
@@ -32,6 +32,6 @@ for dir in docs/locales/*/; do
     rm $targetmdfilepath
     po2md $mdfilepath --md-encoding utf-8 -p $pofilepath -q -w $width -s \
     $targetmdfilepath
-    
+
   done
 done
--- scripts/update-po.sh
+++ scripts/update-po.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-# Automatically generate/update gettext files for 
+# Automatically generate/update gettext files for
 # Localization files in the ./locales/[language]/ folder
 # Prerequisit: pip install mdpo
 
 md2po -v
 
 for dir in docs/locales/*/; do
-  
+
   dir=${dir%*/}
   dir=${dir##*/}
-  
+
   echo "Updating $dir"
   for file in docs/*.md; do
-  
+
     mdfile=${file/docs\//} #Remove docs/
-    mdfilepath=docs/$mdfile #add docs/ 
-  
+    mdfilepath=docs/$mdfile #add docs/
+
     if [ $dir = "en" ]; then
       pofile=${mdfile//.md/.pot} #replace .md with .pot
     else
       pofile=${mdfile//.md/.po} #replace .md with .po
     fi
-  
-    pofilepath=docs/locales/$dir/LC_MESSAGES/$pofile #Add target directory 
+
+    pofilepath=docs/locales/$dir/LC_MESSAGES/$pofile #Add target directory
     #to pofile
     targetmdfile=docs/locales/$dir/$mdfile #Add target directory to mdfile
     echo "Converting $mdfile to $pofile"
